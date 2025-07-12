02f9cdc8afe29d562bb2e6d0cffbf8f630f5194b
Carlos Mendes <carlosmendesjmc@gmail.com>
2025-07-12 18:41:42 -0300

    Adiciona comentários explicativos no código

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+
+// Criando um testo dinamico pra ser utilizado no HTML
+//Criamos uma variavel favoritefood  qu recebe 'pizza'
+//na linha 5 usamos um metodo document.getElementById ('text')
+//com o .innerHTML é envia o valor que tem na variavel favoritefood
 let favoritefood = 'pizza'
-document.getElementById('text').innerHTML = favoritefood
+document.getElementById('text').innerHTML = favoritefood  //se preferir pode usar o valor entre '' sem a variavel tambem
+//vai no document busca o id chamado text e insere o que esta na variavel favoritfood
+
+
